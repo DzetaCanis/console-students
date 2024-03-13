a6f797622c226d624625783e78bb32a19fd90ac9
Darina Malysko <maldarina04@gmail.com>
2024-03-13 22:17:29 +0200

    Implements SubjectEntity, TeacherEntity. Added NUnit-tests for TeacherEntity.

--- Groups/Implementions/TeacherEntity.cs
+++ Groups/Implementions/TeacherEntity.cs
@@ -2,7 +2,17 @@ using Groups.Implementions;
 
 namespace Students.Implementations;
 
-public class TeacherEntity
+public class TeacherEntity : PersonEntity
 {
-   
+    public int Id { get; set; }
+    public string Surname { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    
+    public List<SubjectEntity> Subjects { get; private set; }
+
+    public TeacherEntity(int id, string name, string email) : base(id, name, email)
+    {
+        Subjects = new List<SubjectEntity>();
+    }
 }
